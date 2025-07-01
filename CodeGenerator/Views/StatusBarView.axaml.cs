fb13814365d491085c36b6bc851b72b7ce4d1b0c
Roman Krysyaev <roman-krysyaev@protonmail.com>
2025-07-01 15:26:21 +0300

    Updated project structure.

--- CodeGenerator/Views/StatusBarView.axaml.cs
+++ CodeGenerator/Views/StatusBarView.axaml.cs
@@ -2,7 +2,7 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 
-namespace CodeGenerator;
+namespace CodeGenerator.Views;
 
 public partial class StatusBarView : UserControl
 {
